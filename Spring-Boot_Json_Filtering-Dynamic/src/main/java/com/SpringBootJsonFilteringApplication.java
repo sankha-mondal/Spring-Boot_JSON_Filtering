37e7e28f6c1461a2217c64226ab2ae2b84e385be
sankhamondal400 <sankhasubhra.mondal@wipro.com>
2023-09-16 13:51:04 +0530

    Spring-Boot Json Filtering Dynamic

---
+++ Spring-Boot_Json_Filtering-Dynamic/src/main/java/com/SpringBootJsonFilteringApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsonFilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJsonFilteringApplication.class, args);
+		System.out.println("Spring-Boot Json Filtering Application running on port No 8585..");
+	}
+		//  Add @JsonIgnore w r t field-attribute
+}
---
+++ Spring-Boot_Json_Filtering-Dynamic/src/main/java/com/controller/JsonFilter_Controller.java
@@ -0,0 +1,44 @@
+package com.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.entity.JsonFilter;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
+@RestController  //  localhost:8585
+public class JsonFilter_Controller {
+
+	// http://localhost:8585/filtering
+	@GetMapping("/filtering")
+	public MappingJacksonValue filtering() {
+		
+		JsonFilter jf = new JsonFilter("Sankha", "S@123", "Kolkata");
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(jf);
+		
+		SimpleBeanPropertyFilter filter = 
+				SimpleBeanPropertyFilter.filterOutAllExcept("name","location");
+		
+		FilterProvider provider =
+				new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		
+		mappingJacksonValue.setFilters(provider);
+		
+		return mappingJacksonValue;
+	}
+	
+	
+	// http://localhost:8585/filtering_list
+	@GetMapping("/filtering_list")
+	public List<JsonFilter> filtering_list()  {
+		return Arrays.asList(new JsonFilter("Sankha", "S@123", "Kolkata"),
+							 new JsonFilter("Akash", "A@123", "Chennai"),
+							 new JsonFilter("Vijay", "V@123", "Bangalore"));
+	}
+}
