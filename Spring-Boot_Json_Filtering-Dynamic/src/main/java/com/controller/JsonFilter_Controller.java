e65d1c81c70179b75c507965ca5709455fcb7cd6
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2023-09-16 20:39:50 +0530

    Spring-Boot Json Filtering Dynamic

--- Spring-Boot_Json_Filtering-Dynamic/src/main/java/com/controller/JsonFilter_Controller.java
+++ Spring-Boot_Json_Filtering-Dynamic/src/main/java/com/controller/JsonFilter_Controller.java
@@ -36,9 +36,11 @@ public class JsonFilter_Controller {
 	
 	// http://localhost:8585/filtering_list
 	@GetMapping("/filtering_list")
-	public List<JsonFilter> filtering_list()  {
-		return Arrays.asList(new JsonFilter("Sankha", "S@123", "Kolkata"),
+	public MappingJacksonValue filtering_list()  {
+ 		List<JsonFilter> jf_list = Arrays.asList(new JsonFilter("Sankha", "S@123", "Kolkata"),
 							 new JsonFilter("Akash", "A@123", "Chennai"),
 							 new JsonFilter("Vijay", "V@123", "Bangalore"));
+		
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(jf_list);
 	}
 }
